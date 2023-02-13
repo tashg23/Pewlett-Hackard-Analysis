ed6689b18903c6a11543e6aaa338ae71a4d3c068
nitashagill23@gmail.com <nitashagill23@gmail.com>
2023-02-13 11:46:17 -0500

    Completed deliverable 2

--- Queries/employee_db_challenge.sql
+++ Queries/employee_db_challenge.sql
@@ -28,6 +28,24 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY count(ut.title) DESC;
 
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date, 
+	de.from_date, 
+	de.to_date, 
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
+
 
 
 
