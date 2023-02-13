f3d0f56706f810e90715cc3e1ca1375c766b386e
nitashagill23@gmail.com <nitashagill23@gmail.com>
2023-02-13 12:38:34 -0500

    Fixed unique.csv table

--- Queries/employee_db_challenge.sql
+++ Queries/employee_db_challenge.sql
@@ -1,3 +1,4 @@
+-- Create list of retirement ready employees born between 1952 and 1955
 SELECT e.emp_no, 
 	e.first_name, 
 	e.last_name,
@@ -11,6 +12,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+-- Remove duplicate employees
 SELECT DISTINCT ON (rt.emp_no) 
 	rt.emp_no,
 	rt.first_name, 
@@ -21,6 +23,7 @@ FROM retirement_titles AS rt
 WHERE (rt.to_date = '9999-01-01')
 ORDER BY rt.emp_no ASC, rt.to_date DESC;
 
+-- Find count of how many retirement ready employees by title 
 SELECT COUNT(ut.title),
 	ut.title
 INTO retiring_titles
@@ -28,6 +31,7 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY count(ut.title) DESC;
 
+-- Find senior employees for mentorship program
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no, 
 	e.first_name, 
